fa867c47d5d9d22c5fa462e03ee0b95be45000df
thgilcp <thgilcp@gmail.com>
2020-10-07 00:18:55 +0900

    Update: Spotify Auth

--- DiVVe.API/Spotify.cs
+++ DiVVe.API/Spotify.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.IO;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 
 using ConsoleAppFramework;
@@ -26,44 +29,41 @@ namespace DiVVe.API
         public async Task Auth()
         {
             if (string.IsNullOrEmpty(ClientId))
-            {
                 throw new NullReferenceException(nameof(ClientId));
-            }
 
-            if (File.Exists(CredentialsPath))
-            {
-                await this.Start();
-            }
-            else
-            {
-                await this.StartAuthentication();
-            }
+            var spotify = File.Exists(CredentialsPath) ? await this.GetClient() : await this.GetClientWithAuth();
         }
 
-        private async Task Start()
+
+        private async Task<SpotifyClient> GetClient(PKCETokenResponse? token = null)
         {
-            var json = await File.ReadAllTextAsync(CredentialsPath);
-            var token = JsonSerializer.Deserialize<PKCETokenResponse>(json);
+            if (token is null)
+            {
+                using var stream = new FileStream(CredentialsPath, FileMode.Open, FileAccess.Read, FileShare.Read);
+                token = await JsonSerializer.DeserializeAsync<PKCETokenResponse>(stream);
+            }
 
             var authenticator = new PKCEAuthenticator(ClientId!, token);
-            authenticator.TokenRefreshed += (sender, token) => File.WriteAllBytes(CredentialsPath, JsonSerializer.Serialize(token));
+            authenticator.TokenRefreshed += async (sender, token) =>
+            {
+                using var stream = new FileStream(CredentialsPath, FileMode.Create, FileAccess.Write, FileShare.None);
+                await JsonSerializer.SerializeAsync(stream, token);
+            };
 
             var config = SpotifyClientConfig.CreateDefault()
               .WithAuthenticator(authenticator);
 
             var spotify = new SpotifyClient(config);
-
             var me = await spotify.UserProfile.Current();
-            this.Context.Logger.ZLogInformation($"Welcome {me.DisplayName} ({me.Id}), you're authenticated!");
+            this.Context.Logger.ZLogInformation($"Login: {me.DisplayName} ({me.Id})");
 
-            var playlists = await spotify.PaginateAll(await spotify.Playlists.CurrentUsers().ConfigureAwait(false));
-            this.Context.Logger.ZLogInformation($"Total Playlists in your Account: {playlists.Count}");
-
-            await server.Stop();
+            return spotify;
         }
 
-        private async Task StartAuthentication()
+        private async Task<SpotifyClient> GetClientWithAuth()
         {
+            var source = new TaskCompletionSource<SpotifyClient>();
+
             var (verifier, challenge) = PKCEUtil.GenerateCodes();
 
             await server.Start();
@@ -74,8 +74,9 @@ namespace DiVVe.API
                   new PKCETokenRequest(ClientId!, response.Code, server.BaseUri, verifier)
                 );
 
-                await File.WriteAllBytesAsync(CredentialsPath, JsonSerializer.Serialize(token));
-                await this.Start();
+                using var stream = new FileStream(CredentialsPath, FileMode.Create, FileAccess.Write, FileShare.None);
+                await JsonSerializer.SerializeAsync(stream, token);
+                source.SetResult(await this.GetClient(token));
             };
 
             var request = new LoginRequest(server.BaseUri, ClientId!, LoginRequest.ResponseType.Code)
@@ -92,6 +93,7 @@ namespace DiVVe.API
             };
 
             var uri = request.ToUri();
+
             try
             {
                 BrowserUtil.Open(uri);
@@ -100,6 +102,8 @@ namespace DiVVe.API
             {
                 this.Context.Logger.ZLogInformation("Unable to open URL, manually open: {0}", uri);
             }
+
+            return await source.Task;
         }
     }
 }
